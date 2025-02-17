9c696bb75a943ede8b9763d0df327d24757f1162
saravanakumarshan <saravan@ilife.tech>
2025-02-17 14:41:47 +0530

    updating path of the job

--- runTests.sh
+++ runTests.sh
@@ -28,7 +28,7 @@ echo "Generating Allure report..."
 allure generate --clean allure-results -o allure-report
 
 # Define the Report URL (hosted via Jenkins)
-JENKINS_URL="http://your-jenkins-server/job/sample/allure-report"
+JENKINS_URL="http://localhost:8080/job/sample/allure-report"
 
 # Replace {REPORT_URL} placeholder in email body
 EMAIL_BODY=$(echo "$EMAIL_BODY" | sed "s|{REPORT_URL}|$JENKINS_URL|g")
